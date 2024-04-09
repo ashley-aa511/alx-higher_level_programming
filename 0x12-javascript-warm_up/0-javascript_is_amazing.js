b6f12293ec64008039a2c2d28eada559aa062ee8
ashley-aa511 <okeyoashley@gmail.com>
2024-04-08 23:28:20 -0700

    Javascript warm-up

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// print javascript is amaizing using a const variable
+
+const myVar = 'Javascript is amaizing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// print 3 lines of code
+
+const isFun = 'C is fun';
+const isCool = 'Python is cool';
+const isAmaizing = 'Javascript is amazing';
+console.log(isFun);
+console.log(isCool);
+console.log(isAmaizing);
