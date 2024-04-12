2e4a513d52e3e6ea9677aeafdd034a7f351d5dec
ashley-aa511 <okeyoashley@gmail.com>
2024-04-11 23:34:59 -0700

    argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // Prints the first argument passed to it
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log('No argument');
+// Check if there is at least one argument
+if (process.argv[2]) {
+  console.log(process.argv[2]); // Print the first argument
 } else {
-  console.log(args[0]);
+  console.log('No argument'); // Print if no argument is passed
 }
